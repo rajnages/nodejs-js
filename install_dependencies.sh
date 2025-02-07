e73982d14d60f3a0c6df49e0911856b419901385
Rajavarapu Nageswarao <nagesrajavarapu@gmail.com>
2025-02-07 09:07:06 +0530

    ########

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
-echo "Installing dependencies..."
-cd /var/www/html/nodejs-app
-npm install
+# #!/bin/bash
+# echo "Installing dependencies..."
+# cd /var/www/html/nodejs-app
+# npm install

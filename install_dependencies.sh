04f8f7387ee8b22360d0934467a88324fbf0ef51
Rajavarapu Nageswarao <nagesrajavarapu@gmail.com>
2025-02-07 09:37:12 +0530

    ########

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 sudo echo "Installing dependencies..."
+sudo mkdir -p /var/www/html/nodejs-app
+sudo chown -R root:root /var/www/html/nodejs-app
 sudo cd /var/www/html/nodejs-app
 sudo npm install
--- start_server.sh
+++ start_server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo echo "Starting Node.js server..."
 # Ensure PM2 is installed globally
+sudo cd /var/www/html/nodejs-app
 sudo npm install -g pm2
 # Start the application
 sudo pm2 start /var/www/html/nodejs-app/app.js --name "nodejs-app"
